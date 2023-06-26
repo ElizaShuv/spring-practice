e78980840b919cbf6dd0faca43e87211797c1f8b
ElizaShuv <Shuvlizaveta27@gmail.com>
2023-06-26 07:32:49 +0300

    Добавлено задание по пятой теме

---
+++ 5-spring-boot-and-mvc/src/main/java/ru/sber/controllers/MainController.java
@@ -0,0 +1,13 @@
+package ru.sber.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/home")
+    public String home() {
+        return "page.html";
+    }
+}
